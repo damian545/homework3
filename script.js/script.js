f444a9c8bdab7d9466e9bc582de74623486abcf0
Damian Tapp <dam180@hotmail.com>
2020-03-06 17:24:26 +1100

    adding changes to my homework3

--- script.js/script.js
+++ script.js/script.js
@@ -1,13 +1,5 @@
 <script>
 
-    for (let i = 0; i < length; i += typesCount) {
-      typesArray.forEach(type => {
-        const funcName = Object.keys(type)(0);
-        console.log("funcName: ", funcName);
-
-        generatedPassword += randomFunc[funcName]();
-      });
-    }
     function getRandomLower() {
       return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
       var num = Math.floor(Math.random() * 9) + 1;
@@ -30,10 +22,34 @@
       const symbols = "!@#$%^&*()+=?";
       return symbols[Math.floor(Math.random() * symbols.length)];
       var num = Math.floor(Math.random() * 9) + 1;
+      
     }
     console.log(getRandomSymbol());{
     const floating_btn = document.querySelector(".floating-btn");
     const close_btn = document.querySelector(".close-btn");
     const social_panel_container = document.querySelector(const social_panel_container = document.querySelector)
     }
+    if (typescount === 0){
+      return "";
+    }
+     for (let i = 0; i < length; i += typesCount) {
+      typesArray.forEach(type => {
+        const funcName = Object.keys(type)(0);
+        console.log("funcName: ", funcName);
+
+        generatedPassword += randomFunc[funcName]();
+      })
+    }
+    const finalPassword= generatedPassword.slice(0, length);
+    return finalPassword;
+
+    const floating_btn= document.querySelector('.floating-btn');
+    const close_btn=document.querySelector('.close_btn');
+    const social_panel_container= document.querySelector(".social_panel_container");
+    floating_btn.addEventListener('click', ()=>{
+      social_panel_container.classList.toggle("visible")
+    });
+    close_btn.addEventListener('click', ()=>{
+      social_panel_container.classList.remove("visible")
+    });
     </script>
